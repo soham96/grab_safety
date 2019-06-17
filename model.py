2254b2d68bcf043155cd3d262af99bd11eddd327
soham <96soham96@gmail.com>
2019-06-17 15:46:22 +0530

    added another model

--- model.py
+++ model.py
@@ -1,20 +1,45 @@
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.layers import Dropout
-from keras.layers import LSTM
+from keras.layers import Flatten, Conv1D, Dense, TimeDistributed, ConvLSTM2D, Dropout, LSTM, MaxPooling1D
 from keras.utils import to_categorical
 
 from preprocess import *
 
 data_df, label_df=read_data()
 
-model = Sequential()
-model.add(LSTM(100, input_shape=(1200,11)))
-model.add(Dropout(0.5))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(2, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# fit network
-model.fit_generator(prepare_data_for_training(data_df, label_df, seq_len=1200), steps_per_epoch=1000, epochs=5, verbose=1)
+def LSTM_train():
+    model = Sequential()
+    model.add(LSTM(100, input_shape=(1200,11)))
+    model.add(Dropout(0.5))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(2, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # fit network
+    model.fit_generator(prepare_data_for_training(data_df, label_df, seq_len=1200), steps_per_epoch=1000, epochs=5, verbose=1)
+
+def CNN_LSTM():
+    model = Sequential()
+    model.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu'), input_shape=(None,120,11)))
+    model.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu')))
+    model.add(TimeDistributed(Dropout(0.5)))
+    model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
+    model.add(TimeDistributed(Flatten()))
+    model.add(LSTM(100))
+    model.add(Dropout(0.5))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(2, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit_generator(prepare_data_for_training(data_df, label_df, seq_len=1200, model_type='CNN_LSTM', batch_size=1), steps_per_epoch=1000, epochs=5, verbose=1)
+
+def Conv_LSTM():
+    model = Sequential()
+    model.add(ConvLSTM2D(filters=64, kernel_size=(1,3), activation='relu', input_shape=(n_steps, 1, n_length, n_features)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(n_outputs, activation='softmax'))
+
+if __name__=='__main__':
+    CNN_LSTM()
+    Conv_LSTM()
--- preprocess.py
+++ preprocess.py
@@ -9,17 +9,39 @@ from keras.utils import to_categorical
 from sklearn.preprocessing import RobustScaler
 
 def get_files(file_path):
+    '''
+    Given a folder, it returns the path of all csv files in that folder
+    Args:
+        file_path: Path of the directory containing the csv files
+    Returns:
+        List of all the csv files in the directory
+    '''
     file_list=glob.glob(os.path.join(file_path, '*.csv'))
 
     return file_list
 
 def get_df(file_path_list):
+    '''
+    Given a list of csv files, it returns a pandas dataframe of the csv file.
+    Args:
+        file_path_list: List of all file paths to read
+    Returns:
+        concated dataframe of all the csv files
+    '''
     df=pd.DataFrame()
     for file_path in file_path_list:
         df=pd.concat((df, pd.read_csv(file_path)), ignore_index=True)
     return df
 
 def fix_length(df, seq_len):
+    '''
+    Standardises the length for sequence learning
+    Args:
+        df: DataFrame to standardise
+        seq_len: The sequence length to use
+    Returns:
+        DataFrame with standardised length
+    '''
     cols = ['bookingID', 'Accuracy', 'Bearing', "acceleration_x","acceleration_y",\
             "acceleration_z", "gyro_x","gyro_y","gyro_z", 'second', 'Speed']
     if len(df)>=seq_len:
@@ -30,7 +52,16 @@ def fix_length(df, seq_len):
         return df
 
 def data_engineering(df):
-
+    '''
+    Function to perform data engineering.
+    1) Gets the mean of acceleration and gyro for all axis's
+    2) Gets RMS value for acceleration
+    
+    Args:
+        df: DataFrame to perform data engineering on
+    Returns:
+        DataFrame with data engineering done
+    '''
     #Get Acceleration and gyro values to mean
     for i in [ "acceleration_x","acceleration_y", "acceleration_z", "gyro_x","gyro_y","gyro_z"]:
         df[i]=df[i]-df[i].mean()
@@ -41,13 +72,26 @@ def data_engineering(df):
     return df
 
 def read_data():
+    '''
+    Function to read the pickled data and label data
+    '''
     label_file_list=get_files('safety/labels')
     label_df=get_df(label_file_list)
     with open('data_df.pkl', 'rb') as f:
         data_df=pickle.load(f)
     return data_df, label_df
 
-def prepare_data_for_training(data_df, label_df, seq_len=3000):
+def prepare_data_for_training(data_df, label_df, seq_len=3000, batch_size=32, model_type='LSTM'):
+    '''
+    Generator that returns batches of data for trainining models
+    Args:
+        data_df: DataFrame containing the training data
+        label_df: The lable for the data contained in data_df
+        seq_len: The seq_len for one datapoint in data_df. Default: 3000
+        batch_size: The batch size of data to yield per iteration. Default: 32
+    Returns:
+        A generator that yields batches of data in the form (X, one hot encoded label)
+    '''
     bookingIDs=data_df['bookingID'].drop_duplicates().values
     while True:
         final_data=[]
@@ -69,9 +113,12 @@ def prepare_data_for_training(data_df, label_df, seq_len=3000):
                     final_data.append(temp_df.values)
                     #yield np.asarray(final_data), to_categorical(label_list)
                     i=i+1
-                    if i==32:
+                    if i==batch_size:
                         if 1 in label_list and 0 in label_list:
-                            yield np.asarray(final_data), to_categorical(label_list)
+                            if model_type=='LSTM':
+                                yield np.asarray(final_data), to_categorical(label_list)
+                            elif model_type=='CNN_LSTM':
+                                yield np.asarray(final_data).reshape((int(len(np.asarray(final_data))), 10, -1, 11)), to_categorical(label_list)
                             final_data=[]
                             label_list=[]
                             i=0
